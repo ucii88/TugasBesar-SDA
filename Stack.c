839b92808a9cd3fce7bb0f04f57b4b5b4fe05675
hilmimahdani <hilmimahdanib@gmail.com>
2025-06-09 20:48:53 +0700

    menambahkan ADT Stack

---
+++ Stack.c
@@ -0,0 +1,20 @@
+#include "Stack.h"
+#include "tree.h"
+
+void init_stack(Stack* s) {
+    s->top = -1;
+}
+
+void push(Stack* s, char* rute, int level) {
+    if (s->top < MAX_STACK - 1) {
+        s->top++;
+        strcpy(s->items[s->top].rute, rute);
+        s->items[s->top].level = level;
+    }
+}
+
+void pop(Stack* s) {
+    if (s->top >= 0) {
+        s->top--;
+    }
+}
